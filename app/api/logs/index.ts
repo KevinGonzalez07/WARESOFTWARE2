ff007f6dd7bd0f63a92ac4b55d93bf4261cb57d4
KevinGonzalez07 <kevinglz564@gmail.com>
2025-05-25 13:09:17 -0500

    12

--- app/api/logs/index.ts
+++ app/api/logs/index.ts
@@ -1,23 +1,29 @@
 import prisma from "@/backend/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function funciones(req: NextApiRequest, res:NextApiResponse) {
-    if (req.method === 'GET'){
+export default async function funciones(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
         const historial = await prisma.log.findMany();
         return res.status(200).json(historial);
     }
 
-    if (req.method === 'POST'){
-        const {descripcion, fecha} = req.body;
+    if (req.method === 'POST') {
+        const { descripcion, fecha, id_almacen } = req.body;
         try {
             const nuevoLog = await prisma.log.create({
-                data: {descripcion, fecha},
+                data: {
+                    descripcion,
+                    fecha,
+                    almacen: {
+                        connect: { id_almacen: id_almacen }
+                    }
+                },
             });
             return res.status(201).json(nuevoLog);
-        }catch(error){
-            return res.status(500).json({error: 'No se pudo crear el log'})
+        } catch (error) {
+            return res.status(500).json({ error: 'No se pudo crear el log' });
         }
     }
 
-    return res.status(405).json({mensaje: 'Metodo no permitodo'});
+    return res.status(405).json({ mensaje: 'Metodo no permitido' });
 }
