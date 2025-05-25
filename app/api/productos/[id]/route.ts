d33a23ba6c03f2941854c868702344e472a8bcaa
KevinGonzalez07 <kevinglz564@gmail.com>
2025-05-25 12:42:12 -0500

    3.1

--- app/api/productos/[id]/route.ts
+++ app/api/productos/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import prisma from '@/app/api/backend/prisma'
+import prisma from '@/backend/prisma'
 
 export async function PUT(
   req: NextRequest,
